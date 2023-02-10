96ed5ca02be313db8fe6bde8f42ac490ccf4af4f
paramesh <paramesh8321@gmail.com>
2023-02-10 15:38:40 +0000

    intial commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import {Component} from 'react'
+import UserProfile from './components/UserProfile'
+
+import './App.css'
+
+const userDetailsList = [
+  {
+    uniqueNo: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Esther Howard',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 2,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'Floyd Miles',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 3,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'Jacob Jones',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 4,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
+    name: 'Devon Lane',
+    role: 'Software Developer',
+  },
+]
+
+class App extends Component {
+  state = {searchValue: ''}
+
+  userSearchValue = event => {
+    this.setState({searchValue: event.target.value})
+  }
+
+  render() {
+    const {searchValue} = this.state
+    console.log(searchValue)
+    const updatedList = userDetailsList.filter(eachItem =>
+      eachItem.name.includes(searchValue),
+    )
+    return (
+      <div className="app-container">
+        <h1 className="title">Users List</h1>
+        <input type="search" onChange={this.userSearchValue} />
+        <ul className="list-container">
+          {updatedList.map(eachUser => (
+            <UserProfile userDetails={eachUser} key={eachUser.uniqueNo} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default App
